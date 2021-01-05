b06db9c5ce6492570e5f52a2574607c48f8ea119
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-05 20:55:21 +0100

    shortcuts and more

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,7 +7,7 @@ const renderMovies = (filter = '') => {
 	const movieList = document.getElementById('movie-list');
 	
 	if (movies.length === 0) {
-		movieList.classList.remove(visible);
+		movieList.classList.remove('visible');
 		return;
 	} else {
 		movieList.classList.add('visible');
@@ -27,7 +27,7 @@ const renderMovies = (filter = '') => {
 		//getFormattedTitle = getFormattedTitle.bind(movie);
 		let text = getFormattedTitle.call(movie) + ' - ';
 		for (const key in info) {
-			if (key !== 'title') {
+			if (key !== 'title' && key !== '_title') {
 				text = text + `${key}: ${info[key]}`;
 			}
 		}
@@ -42,29 +42,44 @@ const addMovieHandler = () => {
 	const extraValue = document.getElementById('extra-value').value;
 
 	if (
-		title.trim() === '' ||
 		extraName.trim() === '' ||
 		extraValue.trim() === ''
 	) {
 		return;
 	}
 	
+	this 
+	
 	const newMovie =  {
 		info: {
-			title,
+			set title(val) {
+				if (val.trim() === '') {
+					this._title = 'DEFAULT';
+					return;
+				}
+				this._title = val;
+			},
+			get title() {
+				return this._title;
+			},
 			[extraName]: extraValue
 		},
 		id: Math.random().toString(),
 		getFormattedTitle() {
+			console.log(this);
 			return this.info.title.toUpperCase();
 		}
 	};
+	
+	newMovie.info.title = title;
+	console.log(newMovie.info.title);
 	
 	movies.push(newMovie);
 	renderMovies();
 };
 
 const searchMovieHandler = () => {
+	console.log(this);
 	const filterTerm = document.getElementById('filter-title').value;
 	renderMovies(filterTerm);
 };
